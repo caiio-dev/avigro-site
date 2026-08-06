a00b53f72fa94523562bab9d9f1f09bee8a9b45e
Caio Cardoso <caioconceicao.pro@gmail.com>
2026-08-06 17:14:45 -0300

    feat: implementa roteamento inicial

---
+++ app/Core/Router.php
@@ -0,0 +1,23 @@
+<?php
+
+class Router
+{
+    private array $routes = [];
+
+    public function get(string $path, callable $callback): void
+    {
+        $this->routes['GET'][$path] = $callback;
+    }
+
+    public function dispatch(string $uri, string $method): void
+    {
+        if (isset($this->routes[$method][$uri])) {
+            call_user_func($this->routes[$method][$uri]);
+            return;
+        }
+
+        http_response_code(404);
+
+        echo "Página não encontrada.";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "AVIGRO - MVC funcionando!";
+require_once __DIR__ . '/../app/Core/Router.php';
+
+$router = new Router();
+
+$router->get('/', function () {
+    echo "AVIGRO - MVC funcionando!";
+});
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$basePath = '/Site_Avigro_MVC/public';
+
+$uri = str_replace($basePath, '', $uri);
+
+$uri = $uri ?: '/';
+
+$router->dispatch($uri, $_SERVER['REQUEST_METHOD']);
